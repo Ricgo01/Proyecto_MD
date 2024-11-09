b957ec76b8cadf948e58202d515eccd3b6ab5eeb
Vann06 <127684976+Vann06@users.noreply.github.com>
2024-11-09 16:45:36 -0600

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,7 +1,21 @@
+# Universidad del Valle de Guatemala
+# Facultad de Matematica 
+# Matematica Discreta 
+# Proyecto 2 - Criptografia con RSA
+# 
+# Autores:
+#   Ricardo Godinez - 23247
+#   Vianka Castro - 23201
+# 
+# Fecha:
+#   9/11/2024
+# #
 import random
 import sympy
 import numpy as np
 
+
+#Genera un numero primo de un rango exclusivo 
 def generar_primo(rango_inferior, rango_superior):
     lista_Primos = list(sympy.primerange(rango_inferior,rango_superior +1))
 
@@ -12,6 +26,7 @@ def generar_primo(rango_inferior, rango_superior):
         print("Error, no se pude encontrar un numero primo en el rango seleccionado")
         return None
 
+# Calcula el máximo común divisor (MCD) de dos números usando el algoritmo de Euclides
 def mcd(a, b):
     if b == 0:
         return abs(a) 
@@ -22,6 +37,7 @@ def mcd(a, b):
         r = a % b 
     return abs(b) 
 
+# Calcula los coeficientes de Bézout para dos números a y b
 def bezout(a, b):
     M = np.array([[1, 0], [0, 1]])
 
@@ -43,6 +59,7 @@ def bezout(a, b):
     
     return a, x, y
 
+# Calcula el inverso modular de 'e' módulo 'n' utilizando los coeficientes de Bézout
 def inverso_modular(e, n):
     mcd, x, y = bezout(e, n) 
 
@@ -55,4 +72,89 @@ def inverso_modular(e, n):
 
     return x
 
+# Genera un par de claves RSA (pública y privada) en base a un rango para seleccionar números primos
+def generar_llaves(rango_inferior, rango_superior):
 
+    p = q = generar_primo(rango_inferior, rango_superior)
+    while p == q:
+        q = generar_primo(rango_inferior, rango_superior)
+
+    n = p * q 
+    tot = (p - 1) * (q - 1)
+
+    e = random.randint(2, tot - 1)
+    while mcd(e, tot) != 1:
+        e = random.randint(2, tot - 1)
+
+    d = inverso_modular(e, tot)
+    if d is None:
+        return "Error: No se pudo encontrar el inverso modular"
+
+    tupla_pub = (int(e), int(n))
+    tupla_priv = (int(d), int(n))
+    return tupla_pub, tupla_priv
+    
+# Encripta un mensaje usando la clave pública
+def encriptar(caracter, llave_publica):
+    e, n = llave_publica
+    
+    if caracter >= n:
+        print("Error: El mensaje es muy largo para ser encriptado")
+        return None
+    
+    caracter_encriptado = pow(caracter, e, n)
+    return caracter_encriptado
+        
+# Desencripta un mensaje cifrado usando la clave privada
+def desencriptar(mensaje_encriptado, llave_privada):
+    d,n = llave_privada
+    
+    if mensaje_encriptado >= n:
+        print("Error: El mensaje encriptado es incorrecto")
+        return None
+    
+    mensaje_desencriptado = pow(mensaje_encriptado, d, n)
+    return mensaje_desencriptado
+
+def main():
+    llave_publica = None
+    llave_privada = None
+    mensaje_encriptado = None
+
+    while True:
+        print("\n===== Programa RSA =====")
+        print("MENU")
+        print("1. Iniciar")
+        print("2. Salir")
+
+        opcion = input("Ingrese una opcion > ")
+
+        if opcion == "1":
+            print("----- Generar Llaves ----")
+            rango_inferior = int(input("Ingrese el rango inferior > "))
+            rango_superior = int(input("Ingrese el rango superior > "))
+            llave_publica, llave_privada = generar_llaves(rango_inferior, rango_superior)
+            
+            if llave_publica and llave_privada:
+                print("Llave publica:", llave_publica)
+                print("Llave privada:", llave_privada)
+
+                # Encriptar e
+                mensaje = int(input("Ingresa el mensaje a encriptar : "))
+                mensaje_encriptado = encriptar(mensaje, llave_publica)
+                if mensaje_encriptado is not None:
+                    print("Mensaje encriptado:", mensaje_encriptado)
+
+                # Desencriptar
+                mensaje_descifrado = desencriptar(mensaje_encriptado, llave_privada)
+                if mensaje_descifrado is not None:
+                    print("Mensaje desencriptado:", mensaje_descifrado)
+
+        elif opcion == "2":
+            print("Saliendo del programa...")
+            break
+
+        else:
+            print("Opción no válida. Intenta de nuevo.")
+
+main()
